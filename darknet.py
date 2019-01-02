f9da1391e69eed689ccecaf007c8521af46da9d7
dn0418 <daniel.noble0418@gmail.com>
2019-01-02 17:00:25 +0100

    feat: Add forward function for route and shortcut layers

--- darknet.py
+++ darknet.py
@@ -182,4 +182,25 @@ class Darknet(nn.Module):
 
       if module_type == "convolutional" or module_type == "upsample":
         x = self.module_list[i](x)
-        
+
+      elif module_type == "route":
+        layers = module["layers"]
+        layers = [int(a) for a in layers]
+
+        if (layers[0]) > 0:
+          layers[0] = layers[0] - i
+
+        if len(layers) == 1:
+          x = outputs[i + (layers[0])]
+        else:
+          if (layers[1]) > 0:
+            layers[1] = layers[1] - i
+
+          map1 = outputs[i + layers[0]]
+          map2 = outputs[i + layers[1]]
+
+          x = torch.cat((map1, map2), 1)
+
+      elif  module_type == "shortcut":
+        from_ = int(module["from"])
+        x = outputs[i-1] + outputs[i+from_]
