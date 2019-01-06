67b569ace2127b8caff23f8c58a8df07388dd052
dn0418 <daniel.noble0418@gmail.com>
2019-01-06 04:52:09 +0100

    feat: Add NMS loop, IoUs, predictions to the write result function

--- util.py
+++ util.py
@@ -71,8 +71,41 @@ def unique(tensor):
   
   return tensor_res
 
+def bbox_iou(box1, box2):
+  """
+  Returns the IoU of two bounding boxes     
+  """
+  # Get the coordinates of bounding boxes
+  b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
+  b2_x1, b2_y1, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
+    
+  # get the corrdinates of the intersection rectangle
+  inter_rect_x1 =  torch.max(b1_x1, b2_x1)
+  inter_rect_y1 =  torch.max(b1_y1, b2_y1)
+  inter_rect_x2 =  torch.min(b1_x2, b2_x2)
+  inter_rect_y2 =  torch.min(b1_y2, b2_y2)
+    
+  # Intersection area
+  inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min=0)
+ 
+  # Union Area
+  b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
+  b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
+    
+  iou = inter_area / (b1_area + b2_area - inter_area)
+    
+  return iou
+
 def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
   """
+  Outputs a tensor of shape D x 8 where D is the true detections
+  in all of images, each represented by a row.
+
+  Each detection has 8 attributes: index if the image in the batch
+  to which the detection belongs to, 4 corners coordinates, 
+  objectness score, the score of class with max confidence, 
+  index of that class. 
+  
   """
   # for each of the boundring boxes having objectness score below
   # threshold, set attributes to 0
@@ -128,4 +161,60 @@ def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
       continue
 
     # Get the various classes detected in the image
-    img_classes = unique(image_pred_[:,-1]) # -1 index holds the class index
+    # -1 index holds the class index
+    img_classes = unique(image_pred_[:,-1]) 
+
+    for cls in img_classes:
+    # perform NMS
+    
+      # extract the detections of a particular class
+      cls_mask = image_pred_*(image_pred_[:,-1] == cls).float().unsqueeze(1)
+      class_mask_ind = torch.nonzero(cls_mask[:,-2]).squeeze()
+      image_pred_class = image_pred_[class_mask_ind].view(-1,7)
+
+      # sort the detections such that the entry with the maximum objectness
+      # confidence is at the top
+      conf_sort_index = torch.sort(image_pred_class[:,4], descending = True )[1]
+      image_pred_class = image_pred_class[conf_sort_index]
+      
+      #Number of detections
+      idx = image_pred_class.size(0)
+
+      for i in range(idx):
+      # Get the IOUs of all boxes that come after the one we are looking at 
+      # in the loop
+        try:
+          # first input is the boundring box row indexed by the variable i
+          # second input is a tensor of multiple rows of boundring boxes
+          # the output is tensor containing IoUs of the boundring box
+          # IOU(Box 1, Box 2)
+          ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_class[i+1:])
+        except ValueError:
+          break
+        except IndexError:
+          break
+
+        # Zero out all the detections that have IoU > treshhold
+        iou_mask = (ious < nms_conf).float().unsqueeze(1)
+        image_pred_class[i+1:] *= iou_mask       
+
+        # Remove the non-zero entries
+        non_zero_ind = torch.nonzero(image_pred_class[:,4]).squeeze()
+        image_pred_class = image_pred_class[non_zero_ind].view(-1,7)
+
+      batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)      
+
+      #Repeat the batch_id for as many detections of the class cls in the image
+      seq = batch_ind, image_pred_class
+
+      if not write:
+        output = torch.cat(seq,1)
+        write = True
+      else:
+        out = torch.cat(seq,1)
+        output = torch.cat((output,out))
+
+  try:
+    return output
+  except:
+    return 0
